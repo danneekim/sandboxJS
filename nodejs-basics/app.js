25fa4b25f41ca9a295c45e9bb893a3948850c767
Dannee Kim <dannee.kim.dev@gmail.com>
2021-11-19 18:07:56 -0500

    add commonly used node modules

--- nodejs-basics/app.js
+++ nodejs-basics/app.js
@@ -5,21 +5,70 @@
  * variables & fxns defined in file are local & private scope.
 */
 
-/**
- * At runtime - node wraps module in IIFE
+/** At runtime - node wraps module in IIFE
  ** (function(exports, require, module, __filename, __dirname){ ... })
  ** console.log(module);
  ** console.log(__filename);
  ** console.log(__dirname);
-*/ 
+*/
 
+/** Logger Module*/
 const logger = require("./logger");
-logger('message');
+logger('testing logger module');
 
-setTimeout(function(){ console.log("Hello World"); }, 2000); // Hello after 2s delay
+/** Path Module*/
+const path = require('path');
+var pathObj = path.parse(__filename);
+logger(pathObj);
+
+/** OS Module*/
+const os = require('os');
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+/** File System Module*/
+const fs = require('fs');
+
+//sync
+const files = fs.readdirSync('./');
+console.log(files);
+//async (preferred)
+fs.readdir('./', function(err, files){
+  if(err){
+    console.log('Error', err);
+  } else{
+    console.log('Result', files);
+  }
+});
+
+/** Event Module
+ ** capitalized to indicate a Class
+ ** Class - container of defined properties/methods
+ ** Class: Human | Object: John (inst. of Human)
+*/
+const EventEmitter = require('events'); //Class
+const emitter = new EventEmitter(); //Object
+
+// Register a listener
+emitter.on('messageLogged', function(){
+  console.log('Event Listener called');
+});
+
+// Raise an event
+emitter.emit('messageLogged',);
+
+
+
+
+
+
+
+
+
+setTimeout(function(){ logger("Hello World"); }, 2000); // Hello after 2s delay
 clearTimeout();
-
 // setInterval(); //repeated fxn call after delay
 // clearInterval();
-
 
